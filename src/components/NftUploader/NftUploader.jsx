137f6d01bf7769cbe6fad5dd84097edbff75d6b5
yawn <69970183+yawn-c111@users.noreply.github.com>
2022-09-27 16:31:24 +0900

    feat: update ethers etc

--- src/components/NftUploader/NftUploader.jsx
+++ src/components/NftUploader/NftUploader.jsx
@@ -1,7 +1,100 @@
+// NftUploader.jsx
+import { ethers } from "ethers";
+import Web3Mint from "../../utils/Web3Mint.json";
 import { Button } from "@mui/material";
 import React from "react";
+import { useEffect, useState } from 'react'
 import ImageLogo from "./image.svg";
 import "./NftUploader.css";
+import { Web3Storage } from 'web3.storage'
+
+const CONTRACT_ADDRESS =
+  "0x025e5fEf34DDF4DFb49EB2e2A51C6B65675De502";
+
+const NftUploader = () => {
+  /*
+   * ユーザーのウォレットアドレスを格納するために使用する状態変数を定義します。
+   */
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [web3MintContract, setWeb3MintContract] = useState(null);
+  const [remainingNFTs, setRemainingNFTs] = useState("");
+  const [message, setMessage] = useState("If you choose image, you can mint your NFT");
+
+  /*この段階でcurrentAccountの中身は空*/
+  console.log("currentAccount: ", currentAccount);
+  const checkIfWalletIsConnected = async () => {
+    const { ethereum } = window;
+    if (!ethereum) {
+      console.log("Make sure you have MetaMask!");
+      return;
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
+
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account:", account);
+      setCurrentAccount(account);
+
+      let chainId = await ethereum.request({ method: "eth_chainId" });
+      console.log("Connected to chain " + chainId);
+      const goerliChainId = "0x5";
+      if (chainId !== goerliChainId) {
+        alert("You are not connected to the Goerli Test Network!");
+      }
+
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const connectedContract = new ethers.Contract(
+        CONTRACT_ADDRESS,
+        Web3Mint.abi,
+        signer
+      );
+      connectedContract !== web3MintContract && setWeb3MintContract(connectedContract);
+      const _remainingNFTs = 50 - await connectedContract.readCurrentTokenId();
+      _remainingNFTs !== remainingNFTs && setRemainingNFTs(_remainingNFTs);
+      console.log("remaining NFTs:", _remainingNFTs);
+    } else {
+      console.log("No authorized account found");
+    }
+  };
+
+  const connectWallet = async () =>{
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+      /*
+       * ウォレットアドレスに対してアクセスをリクエストしています。
+       */
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      console.log("Connected", accounts[0]);
+      /*
+       * ウォレットアドレスを currentAccount に紐付けます。
+       */
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const renderNotConnectedContainer = () => (
+      <button onClick={connectWallet} className="cta-button connect-wallet-button">
+        Connect to Wallet
+      </button>
+  );
+  /*
+   * ページがロードされたときに useEffect()内の関数が呼び出されます。
+   */
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
 
 const NftUploader = () => {
   return (
